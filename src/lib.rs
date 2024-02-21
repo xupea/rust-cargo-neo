53d41e49d270fb86f087bd9e2f1b0974346aface
XiaoMing Xu <sysu.peter.hsu@gmail.com>
2024-02-21 17:43:52 +0800

    feat: update with thread and channel

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,58 @@
 use rusqlite::Connection;
-use std::ffi::{CStr, CString};
-use std::os::raw::c_char;
+use serde::Serialize;
+use std::{
+    ffi::{c_char, CStr, CString},
+    sync::mpsc::channel,
+    thread::{self, sleep}, time::Duration,
+};
 
+#[derive(Debug, Serialize)]
+struct Conversation {
+    first_name: String,
+    last_name: String,
+}
+
+/// # Safety
 #[no_mangle]
-pub extern "C" fn get_latest_sessions_from_local(
+pub unsafe extern "C" fn test(
     db_file: *const c_char,
     callback: extern "C" fn(*const c_char),
-) {
+) -> u32 {
+    let (sender, receiver) = channel();
     let db_file_str = unsafe { CStr::from_ptr(db_file).to_string_lossy().into_owned() };
-    // 从本地 SQLite 数据库读取会话列表
-    let conn = Connection::open(&db_file_str).expect("Failed to open SQLite database");
-    let mut stmt = conn
-        .prepare("SELECT * FROM conversation ORDER BY id DESC LIMIT 1")
-        .expect("Failed to prepare select statement");
-    let session_list: String = stmt.query_row([], |row| row.get(0)).unwrap_or_default();
 
-    // 将 Rust 字符串转换为 C 字符串
-    let session_list_cstr =
-        CString::new(session_list).expect("Failed to convert session list to C string");
+    thread::spawn(move || {
+        let conn = Connection::open(&db_file_str).expect("Failed to open SQLite database");
+        let mut stmt = conn
+            .prepare("SELECT * FROM conversation")
+            .expect("Failed to prepare select statement");
 
-    // 调用回调函数，将会话列表传递给其他语言
-    callback(session_list_cstr.as_ptr());
+        let session_list = stmt.query_map([], |row| {
+            Ok(Conversation {
+                first_name: row.get(1).unwrap(),
+                last_name: row.get(2).unwrap(),
+            })
+        });
+
+        let mut convers = vec![];
+        for p in session_list.unwrap() {
+            convers.push(p.unwrap());
+        }
+
+        // 将 Rust 字符串转换为 C 字符串
+        let session_list_cstr = CString::new(serde_json::to_string(&convers).unwrap())
+            .expect("Failed to convert session list to C string");
+
+        sender.send(100).unwrap();
+
+        // 调用回调函数，将会话列表传递给其他语言
+        callback(session_list_cstr.as_ptr());
+    });
+
+    while let Err(_) = receiver.try_recv() {
+        // 可以在这里执行其他操作或等待一段时间
+        sleep(Duration::from_millis(10));
+    }
+
+    return 0;
 }
